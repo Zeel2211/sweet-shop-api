d01b93ae3ceb7420fcd0ce931dc0209ab13a612f
zeel <zeel@localhost.localdomain>
2025-07-16 11:16:33 +0530

    Added add_sweet funtion

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 import psycopg2
 def get_connection():
-    
     return psycopg2.connect(
         host="localhost",
         database="sweets",
         user="zeel",
         password="2121"
     )
+    
+def add_sweet(name, category, price, quantity):
+    raise NotImplementedError("Add sweet not implemented yet")
